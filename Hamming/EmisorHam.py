e30ada9bf047e6cbdf9e800856ce196de96c9edd
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2023-08-03 14:53:47 -0600

    hamming done

--- Hamming/EmisorHam.py
+++ Hamming/EmisorHam.py
@@ -3,16 +3,53 @@
 # Cristian Aguirre
 # redes
 
+
+
 def emisor_Hamming(mensaje):
     # leer el txt donde esta el mensaje
     try:
         mensaje = "Hamming\\" + mensaje + ".txt" # para finalidades de ejecución
         with open(mensaje, 'r') as archivo:
             a = archivo.readline().strip()
+        a = a.replace(' ', '')
+
+        # Calcular la cantidad de ceros necesarios para completar el último conjunto
+        ceros_faltantes = (4 - len(a) % 4) % 4
+        a += '0' * ceros_faltantes
+
+        # Separar el mensaje en conjuntos de 4 elementos
+        conjuntos_de_4 = [a[i:i+4] for i in range(0, len(a), 4)]
+
+        retorno = []
+        for y in conjuntos_de_4:
+            retorno.append(procesoHamming(y))
+
+        mensajes = []
+        codes = retorno[0][1]
+        for u in retorno:
+            mensajes.append(u[0])
+        
+        retorno_str = [str(num) for num in mensajes]
+        codes = [str(x) for x in codes]
+        codes = '-'.join(codes)
+        codes = codes.replace('[', '')
+        codes = codes.replace(']', '')
+        codes = codes.replace(', ', '')
+        codes = codes.replace('-', ' ')
+        
+
+
+        # Abrir el archivo en modo escritura ('w') y escribir los números de retorno
+        with open("Hamming//codedMessageHamming.txt", 'w') as archivo:
+            archivo.write(' '.join(retorno_str))
+            archivo.write('\n')
+            archivo.write(codes)
+
     except FileNotFoundError:
         print(f"Error: El archivo '{mensaje}' no fue encontrado.")
         return None
-    
+
+def procesoHamming(a):
     # paso 1 contar bits
     bits = len(a)
     
@@ -28,19 +65,63 @@ def emisor_Hamming(mensaje):
     for i in range(factor):
         casillas_mod.append((2 ** i)-1)
     
+    retorno = []
+    casillas_x = []
+    for t in range(bits + factor):
+        if t in casillas_mod:
+            retorno.append('x')
+            casillas_x.append(t)
+        else: 
+            retorno.append('y')
+
+    for i in a:
+        for j,h in enumerate(retorno):
+            if retorno[j] == "y":
+                retorno[j] = i
+                break
+
     # escribir tabla
     nueva_tabla = []
     for i in casillas_mod:
         listita = []
+        unos = []
         for j in range(temp_total):
             if (int((j/(i+ 1)) % 2)) == 0:
                 listita.append(0)
             else:
                 listita.append(1)
 
-        nueva_tabla.append(listita) 
+        nueva_tabla.append((listita,unos)) 
 
-    
-    pass    
+    para_obtener_paridad = []
+    for i in nueva_tabla:
+        a_obtener = []
+        for h,j in enumerate(i[0]):
+            if j == 1:
+                a_obtener.append(h-1)  
+        para_obtener_paridad.append(a_obtener)
 
-emisor_Hamming("mensajeHam")
+    for i in para_obtener_paridad:
+        contz = 0
+        contu = 0
+        for j,h in enumerate(retorno):
+            if j in i:
+                if h == "1":
+                    contu += 1
+                if h == "0":
+                    contz += 1
+
+        for y in i:
+            if retorno[y] == "x":
+                if contz < contu:
+                    retorno[y] = 0
+                if contu < contz:
+                  retorno[y] = 1
+                
+    retorno = retorno[::-1]
+
+    # Juntar los elementos en una cadena
+    hammingCode = ''.join(str(item) for item in retorno)
+    return hammingCode,para_obtener_paridad
+
+emisor_Hamming("mensajeHam_ingresado")
---
+++ Hamming/ReceptorHam.java
@@ -0,0 +1,181 @@
+package Hamming;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class ReceptorHam {
+    public static void main(String[] args) {
+        // Ruta del archivo de texto a leer
+        String rutaArchivo = "Hamming//codedMessageHamming.txt";
+
+        // Lista para almacenar las líneas del archivo
+        List<String> lineasArchivo = new ArrayList<>();
+
+        try {
+            // Abrir el archivo y leer su contenido
+            BufferedReader lector = new BufferedReader(new FileReader(rutaArchivo));
+            String linea;
+
+            while ((linea = lector.readLine()) != null) {
+                lineasArchivo.add(linea);
+            }
+
+            // Cerrar el lector después de leer el archivo
+            lector.close();
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+
+        // Imprimir el contenido del archivo línea por línea
+        String mensajes = lineasArchivo.get(0);
+        String codes = lineasArchivo.get(1);
+        String[] arregloMensajes = mensajes.split(" ");
+        String[] arregloCodes = codes.split(" ");
+
+        // Convertir cada número en una cadena en un arreglo de enteros
+        int[][] arregloMensajesEnteros = convertirArregloEntero(arregloMensajes);
+
+        int[][] arregloMensajesInvertidos = new int[arregloMensajesEnteros.length][];
+        for (int i = 0; i < arregloMensajesEnteros.length; i++) {
+            int[] arregloInvertido = new int[arregloMensajesEnteros[i].length];
+            for (int j = 0; j < arregloMensajesEnteros[i].length; j++) {
+                arregloInvertido[j] = arregloMensajesEnteros[i][arregloMensajesEnteros[i].length - 1 - j];
+            }
+            arregloMensajesInvertidos[i] = arregloInvertido;
+        }
+
+        int[][] arregloCodesEnteros = convertirArregloEntero(arregloCodes);
+        int[] decimalesporMensaje = new int[arregloCodesEnteros.length];
+
+        // descodificar
+        for (int i = 0; i < arregloMensajesInvertidos.length; i++) {
+            
+            int[] resultado_paridad = new int[arregloCodesEnteros.length];
+            int[] contenido = arregloMensajesInvertidos[i];
+            int[][] contenidoPorCodigo = obtenerContenidoDecodificado(contenido, arregloCodesEnteros);
+
+            for (int j = 0; j < contenidoPorCodigo.length; j++) {
+                int[] codigo = contenidoPorCodigo[j];
+                int cantidadUnos = contarUnos(codigo);
+                int cantidadCeros = codigo.length - cantidadUnos;
+                // Verificar si hay par o impar en la cantidad de 1's y 0's
+                if (cantidadUnos % 2 == 0 && cantidadCeros % 2 == 0) {
+                    resultado_paridad[j] = 0;
+                } else {
+                    resultado_paridad[j] = 1;
+                }
+                
+            }
+            
+            String resultado = Arrays.toString(resultado_paridad).replaceAll("\\[|\\]|,|\\s", "");
+            int gg = Integer.parseInt(resultado, 2);
+            decimalesporMensaje[i] = gg;
+        }
+
+        // ahora que se saben que decimales son los de error
+        
+        List<Integer> errores = new ArrayList<>();
+        for (int i = 0; i < decimalesporMensaje.length; i++) {
+            int decimal = decimalesporMensaje[i];
+            if (decimal == 0) {} else {
+                System.out.println("\n-------------------------------------------------------------------\n");
+                System.out.println("Error encontrado en cadena " + i + " del mensaje(s) lee en indice: " + decimal);
+                // Agregar el índice a la lista de errores
+                errores.add(decimal);
+            }
+        }
+
+        // buscar el error
+        if (errores.isEmpty()) {
+            System.out.println("\n-------------------------------------------------------------------\n");
+            System.out.println("Todo bien, no se encontraron errores en el mensaje(s) recibido:");
+            for (String x : arregloMensajes) {
+                System.out.println("     " + x);
+            }
+            System.out.println("\n-------------------------------------------------------------------\n");
+        } else {
+            for (String x : arregloMensajes) {
+                System.out.println("  Mensaje original -> " + x);
+            }
+            for (int i = 0; i < arregloMensajesInvertidos.length; i++) {
+                int[] elemento = arregloMensajesInvertidos[i];
+                int indi = errores.get(i).intValue();
+                if (indi >= 0 && indi < elemento.length) {
+                    int hh = elemento[indi-1];
+                    int nuevo_num = (hh == 0) ? 1 : 0;
+                    System.out.println("     --> Se ha cambiado el bit erroneo " + hh + " por " + nuevo_num + " para corregir.");
+                    elemento[indi-1] = nuevo_num;
+                    arregloMensajesInvertidos[i] = elemento;
+
+                } else {
+                    System.out.println("Error: el índice " + i + " está fuera del rango del arregloMensajes.");
+                }
+            }
+            
+            int[][] arregloMensajesDesinvertidos = new int[arregloMensajesInvertidos.length][];
+            for (int i = 0; i < arregloMensajesInvertidos.length; i++) {
+                int[] arregloDesinvertido = new int[arregloMensajesInvertidos[i].length];
+                for (int j = 0; j < arregloMensajesInvertidos[i].length; j++) {
+                    arregloDesinvertido[j] = arregloMensajesInvertidos[i][arregloMensajesInvertidos[i].length - 1 - j];
+                }
+                arregloMensajesDesinvertidos[i] = arregloDesinvertido;
+            }
+
+            StringBuilder mensajeFinalDesinvertido = new StringBuilder();
+
+            for (int i = 0; i < arregloMensajesDesinvertidos.length; i++) {
+                int[] mensajeDesinvertido = arregloMensajesDesinvertidos[i];
+                for (int j = 0; j < mensajeDesinvertido.length; j++) {
+                    mensajeFinalDesinvertido.append(mensajeDesinvertido[j]);
+                }
+            }
+
+            System.out.println("  Mensaje final -> " + mensajeFinalDesinvertido.toString());
+            System.out.println("\n-------------------------------------------------------------------\n");
+
+        }
+
+        // mensajes de adios
+
+
+    }
+
+    // Método para obtener el contenido por código
+    private static int[][] obtenerContenidoDecodificado(int[] contenido, int[][] arregloCodesEnteros) {
+        int[][] resultado = new int[arregloCodesEnteros.length][];
+        for (int i = 0; i < arregloCodesEnteros.length; i++) {
+            int[] codigo = arregloCodesEnteros[i];
+            resultado[i] = new int[codigo.length];
+            for (int j = 0; j < codigo.length; j++) {
+                resultado[i][j] = contenido[codigo[j]];
+            }
+        }
+        return resultado;
+    }
+
+    // Método para contar la cantidad de 1's en un arreglo
+    private static int contarUnos(int[] arreglo) {
+        int contadorUnos = 0;
+        for (int elemento : arreglo) {
+            if (elemento == 1) {
+                contadorUnos++;
+            }
+        }
+        return contadorUnos;
+    }
+
+    // Método para convertir un arreglo de cadenas de números a un arreglo de arreglos de enteros
+    private static int[][] convertirArregloEntero(String[] arregloCadenas) {
+        int[][] resultado = new int[arregloCadenas.length][];
+        for (int i = 0; i < arregloCadenas.length; i++) {
+            resultado[i] = new int[arregloCadenas[i].length()];
+            for (int j = 0; j < arregloCadenas[i].length(); j++) {
+                resultado[i][j] = Character.getNumericValue(arregloCadenas[i].charAt(j));
+            }
+        }
+        return resultado;
+    }
+}
