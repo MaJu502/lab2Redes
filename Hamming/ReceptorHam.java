75f64e51000d7874b9c003b38d5d5b9602d2acd7
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2023-08-04 02:38:33 -0600

    Update ReceptorHam.java

--- Hamming/ReceptorHam.java
+++ Hamming/ReceptorHam.java
@@ -4,9 +4,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
-
 import java.util.Arrays;
 
 public class ReceptorHam {
