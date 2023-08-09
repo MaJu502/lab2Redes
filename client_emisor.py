54904ce63774b1f72e54a28803b04fff1c350f27
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2023-08-09 00:35:22 -0600

    SOCKETS CONFIG DONE

---
+++ client_emisor.py
@@ -0,0 +1,24 @@
+import socket
+
+import json
+
+data = {
+    "type": 0, #0 es hamming, 1 es CRC
+    "mensaje": "Hola, servidor",
+    "timestamp": 1628957460,
+    "otrosDatos": [1, 2, 3]
+}
+
+HOST = "127.0.0.1"   #IP DEL SERVIDOR 
+PORT = 65123
+
+#Your Code Here - Aplicar algoritmo seleccionado
+
+
+# mensaje_a_enviar = [{cadena binaria + bit de paridad} , {cadena binaria + bit de paridad}, ...]
+#--------------
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    s.sendall(json.dumps(data).encode('utf-8'))
